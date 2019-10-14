0ad0139459bd4a2e6bac8e9d73726500b8dcc5d8
Clemens Kaserer <clemens.kaserer@gmail.com>
2019-10-14 16:43:29 +0200

    S2I Maven builder 1.0.0 based on Centos:8

---
+++ Dockerfile
@@ -0,0 +1,35 @@
+FROM centos:8
+
+ENV BUILDER_CONTEXT_DIR="" \
+    BUILDER_MVN_MIRROR="" \
+    BUILDER_MVN_MIRROR_ALLOW_FALLBACK=false \
+    BUILDER_MVN_OPTIONS=""
+    
+ARG S2IDIR="/home/s2i"
+ARG APPDIR="/deployments"
+ARG JAVA_VERSION="java-11-openjdk"
+ARG MAVEN_VERSION="3.5.4"
+
+LABEL io.k8s.description="S2I Maven Builder (Java: ${JAVA_VERSION}, Maven: ${MAVEN_VERSION})" \
+      io.k8s.display-name="S2I Maven Builder" \
+      io.openshift.tags="builder,java,maven" \
+      io.openshift.s2i.scripts-url="image://${S2IDIR}/bin" \
+      maintainer="Clemens Kaserer <clemens.kaserer@gepardec.com>"
+
+RUN mkdir -p ${APPDIR}/target && \
+    yum install -y \
+        ${JAVA_VERSION} \
+        maven-${MAVEN_VERSION}} && \
+    yum clean all && \
+    chgrp -R 0 ${APPDIR} /etc/maven && \ 
+    chmod -R g+rwX ${APPDIR} /etc/maven
+
+COPY s2i ${S2IDIR}
+RUN chgrp -R 0 ${S2IDIR} && \
+    chmod -R g+rwX ${S2IDIR}
+
+USER 1001
+
+WORKDIR ${APPDIR}
+
+CMD ["${S2IDIR}/bin/run"]
---
+++ usecase.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+### 1) Create a new project
+oc new-project s2i-builder-maven \
+   --display-name="S2I Maven Builder" \
+   --description="This project contains all resources to build the S2I Maven Builder and use 
+                  the builder to compile and run the hello world application. The hello world
+                  application used here is available on github (wildfly/quickstart)."
+
+### 2) create the builder image 
+oc new-build https://github.com/gepardec/openshift-S2I-builder-maven#1.0.0 --name=s2i-builder-maven
+oc logs bc/s2i-builder-maven -f
+
+### 3) use the builder to build your artefact
+oc new-build s2i-builder-maven~https://github.com/wildfly/quickstart#18.0.0.Final \
+    --name=binary-artefact  \
+    --env=BUILDER_CONTEXT_DIR=helloworld \
+    --env=BUILDER_MVN_OPTIONS="-P openshift"
+oc logs bc/binary-artefact -f
+
+### 4) Combine artefact with runtime
+oc new-build --name=runtime --docker-image=jboss/wildfly \
+    --source-image=binary-artefact \
+    --source-image-path=/deployments/target/ROOT.war:. \
+    --dockerfile=$'FROM jboss/wildfly\nCOPY ROOT.war /opt/jboss/wildfly/standalone/deployments/ROOT.war'
+oc logs bc/runtime -f
+
+### 5) Deploy the application
+oc new-app runtime --name=hello-world
+
+### 6) Expose the application
+oc expose svc/hello-world
+
+### 7) Access the application
+oc describe route/hello-world | grep "Requested Host:"
